4b656df9bb15a0cc9c7d1f51462c7f06738aa446
Lumi Jääheimo <lumi.jaaheimo@tuni.fi>
2023-01-05 17:19:48 +0200

    current version of the style edit

---
+++ PLUSSA Style Edit.user.js
@@ -0,0 +1,57 @@
+// ==UserScript==
+// @name         PLUSSA Style Edit
+// @icon    https://ae01.alicdn.com/kf/Hac1a58055c5047cdb91349e91aa208d5k.jpg
+// @match        https://plus.tuni.fi/*
+// @author       Lumiko
+// @version      1.0
+// @description    Userscript with GM_addStyle method.
+// @grant GM_addStyle
+// ==/UserScript==
+
+
+GM_addStyle(".sidebar-collapsed { width:100%; }");
+GM_addStyle(".content, .index { color: #ffffff;}");
+GM_addStyle("body {background-color:#0f0f0f;}");
+GM_addStyle(".content h2, .index h2 {background-color: #22374F;}");
+GM_addStyle("pre {background-color:#0f0f0f;}");
+GM_addStyle(".highlight .o { color: #ffc0cb;}");
+GM_addStyle(".content div.codecomment, .content div.guicomment {background-color:#0f0f0f; box-shadow: 2px 2px 2px 0px #324A21; border-color: #324A21}");
+GM_addStyle("a { color: rgb(212, 183, 236) !important;}");
+GM_addStyle(".content div.codecomment.comment-now-locked { background-color: #22374F;}");
+GM_addStyle(".navbar-default {background-color:#222;}");
+GM_addStyle(".breadcrumb {background-color:#222;}");
+GM_addStyle("code, tt, div.tpd-content code, div.tpd-content tt { color: #bb7676;}");
+GM_addStyle(".breadcrumb > .active { color: #bb7676;}");
+GM_addStyle("img {background: #fff;}");
+GM_addStyle("body {color: #fff;}");
+GM_addStyle(".panel {background-color: #0f0f0f;}");
+GM_addStyle(".table-striped > tbody > tr:nth-of-type(2n+1) {background-color: #18051e;}");
+GM_addStyle(".progress {background-color: #333;}");
+GM_addStyle(".news-panel .folding-list-group-item {background-color: #0f0f0f;}");
+GM_addStyle(".news-panel .folding-list-group-item:hover {background-color: rgb(41, 6, 70);}");
+GM_addStyle(".news-panel .current-instance {background-color: #0f0f0f;}");
+GM_addStyle(".news-panel .current-instance {color: #fff;}");
+GM_addStyle(".alert-success {background-color: #11220a;}");
+GM_addStyle(".alert-success {color: #b9c8b5;}");
+GM_addStyle(".well {background-color: #0f0f0f;}");
+GM_addStyle("body#page-mod-astra-exercise #page-content label, #exercise-page-content label { color: #ffffff;}");
+GM_addStyle(".exercise-summary {background-color: rgb(7, 64, 96) !important;}");
+GM_addStyle(".exercise-summary .navbar-nav > li > a, .exercise-summary .navbar-dropdown > li > a, .exercise-summary .navbar-nav > .active > a, .exercise-summary .navbar-dropdown > .active > a, .exercise-summary .navbar-nav > .active > a:focus, .exercise-summary .navbar-nav > .active > a:hover, .exercise-summary .navbar-nav .navbar-text, .exercise-summary .navbar-dropdown .navbar-text {color: #92aebb;}");
+GM_addStyle(".poi-container.poi-content {background: #3e3d3c;}");
+GM_addStyle(".form-control { color: #ffffff;}");
+GM_addStyle(".form-control {background-color:#0f0f0f;}");
+GM_addStyle(".modal-content {background-color:#0f0f0f;}");
+GM_addStyle(".list-group-item {background-color:#0f0f0f;}");
+GM_addStyle(".pinned-list-group-item>p {background-color:#0f0f0f!important;}");
+GM_addStyle(".frontpage .card-clickable:hover h3 { color: #ffffff;}");
+GM_addStyle(".frontpage .card-clickable:hover p { color: #ffffff;}");
+GM_addStyle(".frontpage .card-body { color: #ffffff;}");
+GM_addStyle("button, input, optgroup, select, textarea { color: #000;}");
+GM_addStyle(".highlight .s2 {background-color:#0f0f0f;}");
+GM_addStyle(".highlight .si {background-color:#0f0f0f;}");
+GM_addStyle(".highlight .sa {background-color:#0f0f0f;}");
+GM_addStyle(".highlight .s1 {color:#0f0f0f;}");
+GM_addStyle(".highlight .ow {color:#ffffff;}");
+
+
+//GM_addStyle(".content div.highlight pre span.loc-now-locked, .content pre.repl span.loc-now-locked, .content pre.repl span.loc-now-locked strong, .content pre.repl span.loc-now-locked em, .content pre.pseudocode span.loc-now-locked, .content pre.pseudocode span.loc-now-locked em, .content div.console pre span.loc-now-locked, .content div.console pre span.loc-now-locked em, .content div.animation div.loc-now-locked { background: #602424;}");
